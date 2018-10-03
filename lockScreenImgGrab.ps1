cb957c32d2d05f3d5c370d8d16fcf0eddbdfcb65
Justin <braham@uoguelph.ca>
2018-10-03 08:55:22 -0400

    Removed errors on duplicate files

--- lockScreenImgGrab.ps1
+++ lockScreenImgGrab.ps1
@@ -4,21 +4,33 @@ $src = "C:\Users\$env:UserName\AppData\Local\Packages\Microsoft.Windows.ContentD
 $dst = "C:\Users\$env:UserName\Pictures\wallpapers"
 
 if(!(Test-Path -Path $dst )) {
-    New-Item -ItemType directory -Path $dst
+  New-Item -ItemType directory -Path $dst
 }
 
 Get-ChildItem $src | Copy-Item -Destination $dst -Force
 
-Get-ChildItem $dst | Rename-item -newname { [io.path]::ChangeExtension($_.name, "png") }
+Get-ChildItem $dst | Foreach-Object {
+  $newName = [io.path]::ChangeExtension($_.name, "png")
+  $dest = Join-Path -Path $_.Directory.FullName -ChildPath $newName
+  Move-Item -Path $_.FullName -Destination $dest -Force
+}
 
 Get-ChildItem $dst | Foreach-Object {
-    $png = New-Object System.Drawing.Bitmap $_.FullName
-
+  $name = $_.FullName
+  try
+  {
+    $png = New-Object System.Drawing.Bitmap $name
     if (!($png.Width.Equals(1920) -and $png.Height.Equals(1080))) {
         $png.Dispose()
-        Remove-Item $_.FullName -Force
+        Remove-Item $name -Force
     }
     else {
         $png.Dispose()
     }
+  }
+  catch
+  {
+    $png.Dispose()
+    Remove-Item $name -Force
+  }
 }
